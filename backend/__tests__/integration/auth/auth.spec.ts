1ae4dcd41daf0c550cb956ade064fbc841232f8e
junix033101 <64710006+junix033101@users.noreply.github.com>
2022-09-04 23:17:27 +0800

    test(schedule): PNRN-49 draft test schedule for post and get routes

--- backend/__tests__/integration/auth/auth.spec.ts
+++ backend/__tests__/integration/auth/auth.spec.ts
@@ -15,7 +15,7 @@ import {
 } from '../fixtures/auth.fixtures';
 import { request, requestWithAdmin, requestWithStaff } from '../setup';
 
-xdescribe('auth.spec.ts - Authentication Controller', () => {
+describe('auth.spec.ts - Authentication Controller', () => {
   let loginRoute: string;
   let registerRoute: string;
   let logoutRoute: string;
