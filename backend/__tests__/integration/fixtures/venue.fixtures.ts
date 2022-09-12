b37cc9cc818fe01a7741ee7b1870143f31f420d9
junix033101 <64710006+junix033101@users.noreply.github.com>
2022-09-12 17:27:35 +0800

    fix-test(venue) changed test cases for unique name and unrequired notes

--- backend/__tests__/integration/fixtures/venue.fixtures.ts
+++ backend/__tests__/integration/fixtures/venue.fixtures.ts
@@ -6,8 +6,13 @@ import { HttpStatus } from '@nestjs/common';
 const venueRoute = VenueController.VENUE_API_PATH;
 
 export const testAddVenue: VenueDTO = {
+  name: 'Canteen',
+  notes: 'this is a test for canteen',
+};
+
+export const testAddVenueSameName: VenueDTO = {
   name: 'Covered Court',
-  notes: 'this is a test for covered court',
+  notes: 'this is a test for court',
 };
 
 export const addVenue = async (dto: VenueDTO): Promise<VenueDTO> => {
@@ -15,6 +20,15 @@ export const addVenue = async (dto: VenueDTO): Promise<VenueDTO> => {
     .post(venueRoute)
     .send(dto)
     .expect(HttpStatus.CREATED);
+
+  return body;
+};
+
+export const addVenueSameName = async (dto: VenueDTO): Promise<VenueDTO> => {
+  const { body } = await requestWithStaff
+    .post(venueRoute)
+    .send(dto)
+    .expect(HttpStatus.BAD_REQUEST);
 
   return body;
 };
