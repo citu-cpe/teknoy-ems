f5cd8d44ea105c89eec71485a81dea542a180ff9
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-09-13 17:08:39 +0800

    feat: add team cards

--- frontend/src/modules/index/components/Dashboard.tsx
+++ frontend/src/modules/index/components/Dashboard.tsx
@@ -1,5 +1,14 @@
-import { Button, Center } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
+import {
+  daxu,
+  jaq,
+  mvprinch,
+  patrek,
+  princh,
+  raol,
+} from '../../../assets/team';
 import { ContentHeader } from '../../../shared/components/content';
+import { TeamCard } from '../../../shared/components/elements/card';
 import { MainLayout } from '../../../shared/components/layout/MainLayout';
 
 export const Dashboard = () => {
@@ -13,7 +22,39 @@ export const Dashboard = () => {
           </Button>
         }
       />
-      <Center>Surpass your limits!</Center>
+      <Flex flexWrap='wrap' w='full' gap={5} justify='center' align='center'>
+        <TeamCard
+          src={princh.src}
+          srcHover={mvprinch.src}
+          title='@yummy_yami'
+          subtitle='MVPrinch'
+          role='Scrum Master | Fullstack | QA'
+        />
+        <TeamCard
+          src={raol.src}
+          title='@magna_kumlodi'
+          subtitle='raol'
+          role='Product Owner | Frontend Development'
+        />
+        <TeamCard
+          src={patrek.src}
+          title='@marie_gration'
+          subtitle='patrek'
+          role='Backend Development'
+        />
+        <TeamCard
+          src={jaq.src}
+          title='@asta_ng_dimalasa'
+          subtitle='jaq'
+          role='QA | Test Manager'
+        />
+        <TeamCard
+          src={daxu.src}
+          title='@grey_border'
+          subtitle='daxu'
+          role='UI/UX Designer'
+        />
+      </Flex>
     </MainLayout>
   );
 };
