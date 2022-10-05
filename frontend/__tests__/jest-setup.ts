9756f5ac04113a0410045e3eb5b50ed0867787b1
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-10-05 23:43:16 +0800

    test(jest): import support for IntersectionObservers

--- frontend/__tests__/jest-setup.ts
+++ frontend/__tests__/jest-setup.ts
@@ -1,1 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
+
+// Add support for IntersectionObservers
+// https://github.com/roderickhsiao/react-in-viewport/issues/71
+import 'w3c/IntersectionObserver@master/polyfill';
