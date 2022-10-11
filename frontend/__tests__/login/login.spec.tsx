249cafb6ce599ddb03e51a8732a143ade438d5d7
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-10-11 16:16:08 +0800

    test: fix component test ekis

--- frontend/__tests__/login/login.spec.tsx
+++ frontend/__tests__/login/login.spec.tsx
@@ -1,54 +1,54 @@
-// import { render, screen, waitFor } from '@testing-library/react';
-// import { Login } from '../../src/modules/login/components/Login';
-// import { QueryClient, QueryClientProvider } from 'react-query';
-// import userEvent from '@testing-library/user-event';
-// import 'intersection-observer';
+import { render, screen, waitFor } from '@testing-library/react';
+import { Login } from '../../src/modules/login/components/Login';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import userEvent from '@testing-library/user-event';
+import 'intersection-observer';
 
-// describe('Login Page', () => {
-//   beforeEach(() => {
-//     const queryClient = new QueryClient();
+describe('Login Page', () => {
+  beforeEach(() => {
+    const queryClient = new QueryClient();
 
-//     render(
-//       <QueryClientProvider client={queryClient}>
-//         <Login />
-//       </QueryClientProvider>
-//     );
-//   });
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Login />
+      </QueryClientProvider>
+    );
+  });
 
-//   it('should have email and password input fields and submit button', () => {
-//     const emailInput = screen.getByRole<HTMLInputElement>('textbox');
-//     const passwordInput = screen.getByLabelText<HTMLInputElement>(/password/i);
-//     const logInButton = screen.getByRole<HTMLButtonElement>('button', {
-//       name: /log in/i,
-//     });
+  it('should have email and password input fields and submit button', () => {
+    const emailInput = screen.getByRole<HTMLInputElement>('textbox');
+    const passwordInput = screen.getByLabelText<HTMLInputElement>(/password/i);
+    const logInButton = screen.getByRole<HTMLButtonElement>('button', {
+      name: /log in/i,
+    });
 
-//     expect(emailInput).toHaveAttribute('type', 'email');
-//     expect(passwordInput).toHaveAttribute('type', 'password');
-//     expect(logInButton).toBeInTheDocument();
-//   });
+    expect(emailInput).toHaveAttribute('type', 'email');
+    expect(passwordInput).toHaveAttribute('type', 'password');
+    expect(logInButton).toBeInTheDocument();
+  });
 
-//   it('should show error message if fields are empty and submit button is clicked', async () => {
-//     const logInButton = screen.getByRole<HTMLButtonElement>('button', {
-//       name: /log in/i,
-//     });
+  it('should show error message if fields are empty and submit button is clicked', async () => {
+    const logInButton = screen.getByRole<HTMLButtonElement>('button', {
+      name: /log in/i,
+    });
 
-//     userEvent.click(logInButton);
+    userEvent.click(logInButton);
 
-//     const errorMessages = await waitFor(() => screen.getAllByText(/required/i));
+    const errorMessages = await waitFor(() => screen.getAllByText(/required/i));
 
-//     expect(errorMessages).toHaveLength(2);
-//   });
+    expect(errorMessages).toHaveLength(2);
+  });
 
-//   it('should show error message when email is invalid', async () => {
-//     const emailInput = screen.getByRole<HTMLInputElement>('textbox');
+  it('should show error message when email is invalid', async () => {
+    const emailInput = screen.getByRole<HTMLInputElement>('textbox');
 
-//     userEvent.type(emailInput, 'test');
-//     userEvent.tab();
+    userEvent.type(emailInput, 'test');
+    userEvent.tab();
 
-//     const errorMessage = await waitFor(() =>
-//       screen.getByText(/invalid email/i)
-//     );
+    const errorMessage = await waitFor(() =>
+      screen.getByText(/invalid email/i)
+    );
 
-//     expect(errorMessage).toBeInTheDocument();
-//   });
-// });
+    expect(errorMessage).toBeInTheDocument();
+  });
+});
