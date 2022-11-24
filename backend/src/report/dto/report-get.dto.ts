d7439076d7726e41cc723deb4d66083dadb5e5dc
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-11-24 22:49:20 +0800

    feat(reports): TEMS-115 implement organizer & date filters (#89)
    Co-authored-by: Princh Canal <princhcanal@gmail.com>

--- backend/src/report/dto/report-get.dto.ts
+++ backend/src/report/dto/report-get.dto.ts
@@ -1,3 +1,4 @@
+import { Type } from 'class-transformer';
 import { IsOptional, IsString, ValidateNested } from 'class-validator';
 import { ReportFilterDTO } from './report-filter.dto';
 
@@ -7,5 +8,6 @@ export class ReportGetDTO {
   public message?: string;
 
   @ValidateNested()
+  @Type(() => ReportFilterDTO)
   public reportFilterDTO: ReportFilterDTO;
 }
